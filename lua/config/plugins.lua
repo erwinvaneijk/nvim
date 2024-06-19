3cec11e35059f39ff96938fd3b583e4d8a44b5c9
Erwin van Eijk <235739+erwinvaneijk@users.noreply.github.com>
2024-06-19 09:46:16 +0200

    chore: update plugins

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -211,6 +211,20 @@ return {
       end, { desc = "trigger linting for current file" })
     end,
   },
+  -- python support
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+    opts = {
+      name = { "venv", ".venv" },
+      auto_refresh = false,
+    },
+    event = "VeryLazy",
+    keys = {
+      { '<leader>vs', '<cmd>VenvSelect<cr>' },
+      { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+    }
+  },
   -- code formatters
   {
     "stevearc/conform.nvim",
